20ac3be2f02e4be16c44aeedf03a28eb400527db
doodad-js <doodad-js@f4e9449a-2e7a-49bc-89ce-d1f85e87716c>
2016-03-22 07:16:47 +0000

    - <Fix> doodad-js: "doodad.UNREGISTER" - <Fix> doodad-js: Isolated interfaces was mixed between object instances. - <Fix> doodad-js: Isolated interfaces was not extensible. - <Fix> doodad-js: "namespaces.loadNamespaces" errors was not properly propagating. - <Fix> doodad-js: "namespaces.loadNamespaces" was initializing modules more than once. - <Fix> doodad-js: "tools.log" hook. - <Fix> doodad-js-http: Folder template errors was not properly propagating. - <Fix> doodad-js-io: (client) "io.stdout" was set to the DOM. - <Fix> doodad-js-io: Default console hook now writes to stderr for errors/warnings/infos and stdout for normal logs. - <Fix> doodad-js-loader: "loader.loadScripts" errors was not properly propagating. - <Fix> doodad-js-make: Command arguments parsing. - <Fix> doodad-js-terminal: Fix for Unicode characters. - <Fix> doodad-js-terminal: Add missing keys mapping. - <Fix> doodad-js-terminal: Fix cursor. - <Fix> doodad-js-terminal: Other bugs. - <Fix> doodad-js-unicode: "charAt". - <Fix> all packages: Package versions. - <Chg> all packages: Attempt to increment support for old browsers. - <Chg> all packages: Requires already minified code instead of using browserify's transforms to minify from source. - <Chg> doodad-js-io: "data.valueOf()" no longer returns "io.EOF" when "data.raw" is "io.EOF". - <Chg> doodad-js-io: "read" method now returns an object with more information than just the value. - <Chg> doodad-js: "namespaces" code review. - <Chg> doodad-js-locale: Database files are now pre-compiled instead of being compiled on the fly. - <Chg> doodad-js-locale: Database files are no longer included in browserify bundles. - <Chg> doodad-js-unicode: Some Unicode specific functions moved inside "doodad-js-locale" package, but still accessibles from "unicode.*". - <Chg> doodad-js-make: Code review - <Chg> doodad-js-safeeval: Never allow some tokens - <Chg> doodad-js-terminal: Code review. - <Chg> doodad-js-widgets: Refactory for client-side vs server-side. - <New> doodad-js: "doodad.RAW_EVENT" - <New> doodad-js: "types.isArrayBuffer", "types.isTypedArray", (nodejs) "types.isBuffer" - <New> doodad-js: (nodejs) "nodejs.Interfaces.IEmitter" - <New> doodad-js-io: (nodejs) "nodejs.IO.Interfaces.IReadable", "nodejs.IO.Interfaces.IWritable", "nodejs.IO.Interfaces.IDuplex" amd "nodejs.IO.Interfaces.ITransform" - <New> doodad-js-make: Can now loads Doodad packages and modules with class "Make.Modules.Load" - <New> doodad-js-make: Class "Make.Browserify.Javascript": Can now minify bundles with new option "minify" - <New> doodad-js-minifiers: Now implements "ITransform" - <New> doodad-js-unicode: "prevChar", "charsCount".

--- trunk/node_modules/doodad-js-safeeval/index.js
+++ trunk/node_modules/doodad-js-safeeval/index.js
@@ -35,7 +35,7 @@
 		DD_MODULES = (DD_MODULES || {});
 		DD_MODULES[MODULE_NAME] = {
 			type: 'Package',
-			version: '0.1.0a',
+			version: '0.2.0a',
 			namespaces: null,
 			dependencies: ['Doodad.Modules'],
 			exports: exports,
